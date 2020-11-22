06987a39bffa1e3823b36709f152b657ce758e3f
RaphyPSW <49653672+RaphyPSW@users.noreply.github.com>
2020-11-21 20:32:23 -0500

    a

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int speed;
+
+    private int waypointIndex;
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Patrolling()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if(waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+}
