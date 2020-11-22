1a6d806ac1f823b0d53780d586a8540c6b34b884
RaphyPSW <49653672+RaphyPSW@users.noreply.github.com>
2020-11-21 21:41:47 -0500

    bon

--- Assets/Scripts/Bread/Drop.cs
+++ Assets/Scripts/Bread/Drop.cs
@@ -6,9 +6,6 @@ public class Drop : MonoBehaviour
 {
     public Patrol patrol;
     public GameObject bread;
-    public int breadAmount;
-    public GameObject[] breadList;
-    BoxCollider[] colliderList;
     BoxCollider collider;
     public Rigidbody rb;
     public Transform npc;
@@ -16,26 +13,11 @@ public class Drop : MonoBehaviour
     public float dropFowardForce;
     public float dropUpwardForce;
     public int dropPoint;
+    public int breadCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        breadAmount = (int)Random.Range(1f, 8f);
-        breadList = new GameObject[breadAmount];
-        colliderList = new BoxCollider[breadAmount];
-        Debug.Log(breadList.Length);
-
-        for(int i = 0; i < breadList.Length; i++)
-        {
-            Instantiate(bread, transform.position, Quaternion.identity);
-            
-        }
-
-        for (int i = 0; i < colliderList.Length; i++)
-        {
-            colliderList[i] = breadList[i].GetComponent<BoxCollider>();
-        }
-
         collider = bread.GetComponent<BoxCollider>();
         collider.isTrigger = true;
         rb.isKinematic = true;
@@ -51,6 +33,7 @@ public class Drop : MonoBehaviour
         //if (Input.GetKeyDown(KeyCode.R))
         if (patrol.waypointIndex == dropPoint && bread.transform.parent != null)
         {
+
             bread.transform.parent = null;
             bread.SetActive(true);
             collider.isTrigger = false;
@@ -65,6 +48,7 @@ public class Drop : MonoBehaviour
             dropFowardForce = Random.Range(2f, 7f);
             dropUpwardForce = Random.Range(2f, 7f);
             dropPoint = (int)Random.Range(1f, 12f);
+
         }
     }
 }
