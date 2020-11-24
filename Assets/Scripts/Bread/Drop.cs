b00545a6acaa61d743636c0f5b57eb84d48ef591
RaphyPSW <49653672+RaphyPSW@users.noreply.github.com>
2020-11-23 20:29:30 -0500

    yeet du pain dans l'etang

--- Assets/Scripts/Bread/Drop.cs
+++ Assets/Scripts/Bread/Drop.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class Drop : MonoBehaviour
 {
     public Patrol patrol;
-    public GameObject bread;
     BoxCollider collider;
-    public Rigidbody rb;
     public Transform npc;
 
     public float dropFowardForce;
@@ -15,27 +13,34 @@ public class Drop : MonoBehaviour
     public int dropPoint;
     public int breadCount;
 
+    private GameObject breadPrefab;
+    private Rigidbody rb;
+    private GameObject currentBread;
+
+    private void Awake()
+    {
+        var bread = Resources.Load("Prefab/Bread");
+        breadPrefab = bread as GameObject;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        collider = bread.GetComponent<BoxCollider>();
-        collider.isTrigger = true;
-        rb.isKinematic = true;
-
+        Spawn();
         dropFowardForce = Random.Range(2f, 7f);
         dropUpwardForce = Random.Range(2f, 7f);
         dropPoint = (int)Random.Range(2f, 12f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //if (Input.GetKeyDown(KeyCode.R))
-        if (patrol.waypointIndex == dropPoint && bread.transform.parent != null)
+        if (patrol.waypointIndex == dropPoint && currentBread.transform.parent != null)
         {
-
-            bread.transform.parent = null;
-            bread.SetActive(true);
+            currentBread.transform.parent = null;
+            currentBread.SetActive(true);
             collider.isTrigger = false;
             rb.isKinematic = false;
 
@@ -45,10 +50,22 @@ public class Drop : MonoBehaviour
             float random = Random.Range(-1f, 1f);
             rb.AddTorque(new Vector3(random, random, random));
 
-            dropFowardForce = Random.Range(2f, 7f);
-            dropUpwardForce = Random.Range(2f, 7f);
-            dropPoint = (int)Random.Range(1f, 12f);
+            dropFowardForce = Random.Range(4f, 7f);
+            dropUpwardForce = Random.Range(4f, 7f);
+            dropPoint = (int)Random.Range(2f, 12f);
 
+            Spawn();
         }
+    }
+
+    private void Spawn()
+    {
+        currentBread = Instantiate(breadPrefab, npc.transform.position, npc.transform.rotation);
+        currentBread.transform.parent = npc;
+        collider = currentBread.GetComponent<BoxCollider>();
+        collider.isTrigger = true;
+        rb = currentBread.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
     }
 }
