a61bd38eaaf25d9a9617526598b02feb53f46949
gosfi <samLef12@outlook.com>
2020-11-28 18:01:07 -0500

    can't mix up which is which player

---
+++ Assets/Scripts/Aesthetic/PlayerIdentifier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerIdentifier : MonoBehaviour
+{
+    [SerializeField] string playerText;
+    [SerializeField] TextMeshPro bigText;
+    [SerializeField] Camera _camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bigText.text = playerText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(_camera.transform.position);
+    }
+}
